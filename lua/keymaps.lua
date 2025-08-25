2f3306cc6657106ba6c1b0592beb1c46512ab172
saiakat <maxstender1@gmail.com>
2025-08-25 10:06:48 +0200

    just found out about gc in v so not needed

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -32,5 +32,3 @@ vim.keymap.set('v', '>', '>gv', opts)
 -- Add tab in front of all selected lines
 vim.keymap.set('v', '<Tab>', '>gv', opts)
 vim.keymap.set('v', '<S-Tab>', '<gv', opts)
-
-vim.keymap.set('v', '<C-\\>', ':s/^/\\/\\/ <CR>', opts)
