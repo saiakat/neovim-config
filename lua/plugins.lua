c02fa182be6c2cd38e41fd1ee0d3510834974a59
saiakat <maxstender1@gmail.com>
2025-08-26 16:17:33 +0200

    removed not working plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -39,11 +39,6 @@ return require('lazy').setup({
   },
   { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   {'Vigemus/iron.nvim'},
-  {
-      'goerz/jupytext.nvim',
-      version = '0.2.0',
-      opts = {},  -- see Options
-  },
   -- completion
   'hrsh7th/nvim-cmp',
   'hrsh7th/cmp-nvim-lsp',
