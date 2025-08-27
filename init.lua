92add95425d7a6a3f35370ba8859efabdeb4922c
saiakat <maxstender1@gmail.com>
2025-08-27 16:42:09 +0200

    removed redundant line in init lua and added function for making background if a file exists

--- init.lua
+++ init.lua
@@ -5,4 +5,21 @@ require('colorscheme')
 require('lsp')
 require('plugins_config')
 require('auto_sync_lazy')
-vim.opt.clipboard = 'unnamedplus'
+
+local function path_exists(path)
+  local file, err = io.open(path, "r")
+  if file then
+    file:close()
+    return true
+  elseif err and err:find("No such file or directory") then
+    return false
+  else
+    -- Handle other potential errors, e.g., permissions
+    return false -- Or handle as needed
+  end
+end
+
+local home = os.getenv("HOME")
+if path_exists(home .. '/.config/nvim/lua/colors.lua') then
+  require('colors')
+end
