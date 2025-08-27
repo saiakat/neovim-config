6163ce7ce00b6ab62abbc4aed0cdfa42e6eb0ec9
saiakat <maxstender1@gmail.com>
2025-08-27 16:11:46 +0200

    removed duplicate line of code

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -18,8 +18,6 @@ require('mason-lspconfig').setup({
   },
 })
 
-require("mason-lspconfig").setup({})
-
 local on_attach = function (_ , _)
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
     vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
