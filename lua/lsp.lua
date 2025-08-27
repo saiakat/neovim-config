aba261a94bd021ffae8f415aa849b22b47ec9450
saiakat <maxstender1@gmail.com>
2025-08-27 16:10:54 +0200

    removed mason file

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,5 +1,21 @@
-require("mason").setup()
-require("mason-lspconfig").setup({})
+require('mason').setup({
+  -- Optional: Configure Mason's general settings here
+})
+require('mason-lspconfig').setup({
+  -- List of LSP servers to automatically install if not present
+  ensure_installed = {
+    "pyright",
+    "lua_ls",
+    "yamlls",
+      -- Add more LSP server names as needed
+  },
+  -- Handlers for configuring LSP servers with nvim-lspconfig
+  handlers = {
+      function(server_name)
+          require('lspconfig')[server_name].setup({})
+      end,
+  },
+})
 
 local on_attach = function (_ , _)
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
