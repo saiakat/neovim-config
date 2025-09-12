413f63547474b92bf5246bce3afcda9bfed6505c
saiakat <maxstender1@gmail.com>
2025-09-12 14:04:27 +0200

    comments on neotest for documentation

--- lua/plugins_config/neotest.lua
+++ lua/plugins_config/neotest.lua
@@ -6,18 +6,22 @@ neotest.setup({
   },
 })
 
+-- run nearest test
 vim.keymap.set("n", "<leader>tc", function()
   neotest.run.run()
 end)
 
+-- run current file
 vim.keymap.set("n", "<leader>tf", function()
   neotest.run.run(vim.fn.expand("%"))
 end)
 
+-- watch test
 vim.keymap.set("n", "<leader>tw", function()
   neotest.watch.toggle(vim.fn.expand("%"))
 end)
 
+-- see test output
 vim.keymap.set("n", "<leader>to", function()
   require("neotest").output.open({ enter = true })
 end)
