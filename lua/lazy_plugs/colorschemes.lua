b125a189a481b0668edfe1d66d851a63067f41e0
stenderm <max.stender@dw.com>
2026-04-27 14:24:34 +0200

    not happy about this but this is the solution for now

--- lua/lazy_plugs/colorschemes.lua
+++ lua/lazy_plugs/colorschemes.lua
@@ -1,5 +1,5 @@
 return {
   {'rose-pine/neovim', name = 'rose-pine'},
   {'catppuccin/nvim', name = 'catppuccin'},
-  {'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
+  {'nvim-treesitter/nvim-treesitter', tag = 'v0.9.3', build = ':TSUpdate'},
 }
