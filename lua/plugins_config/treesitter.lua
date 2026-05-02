682041a08f60f09087c730fda503f79e2a15fe1b
saiakat <maxstender1@gmail.com>
2026-05-02 02:20:31 +0200

    Starting stuff with treesitter now like this apparently....

--- lua/plugins_config/treesitter.lua
+++ lua/plugins_config/treesitter.lua
@@ -8,3 +8,15 @@
 --     },
 -- }
 require('nvim-treesitter').install { "c", "lua", "python", "javascript", "typescript", "html", "css", "json", "bash", "dockerfile", "go", "java", "toml", "vim", "yaml" }
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { "c", "lua", "python", "javascript", "typescript", "html", "css", "json", "bash", "dockerfile", "go", "java", "toml", "vim", "yaml" },
+  callback = function()
+    -- syntax highlighting, provided by Neovim
+    vim.treesitter.start()
+    -- folds, provided by Neovim
+    -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+    -- vim.wo.foldmethod = 'expr'
+    -- indentation, provided by nvim-treesitter
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+  end,
+})
