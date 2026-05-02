04c20274c7905a8a17cd0b7a6c36557f8384a61e
saiakat <maxstender1@gmail.com>
2026-05-02 02:12:23 +0200

    finally fixed it :)

--- lua/plugins_config/treesitter.lua
+++ lua/plugins_config/treesitter.lua
@@ -1,9 +1,10 @@
-require('nvim-treesitter.configs').setup{
-    ensure_installed = { "c", "lua", "python", "javascript", "typescript", "html", "css", "json", "bash", "dockerfile", "go", "rust", "java", "php", "ruby", "toml", "vim", "yaml" },
-
-    sync_install = false,
-    auto_install = true,
-    highlight = {
-        enable = true,
-    },
-}
+-- require('nvim-treesitter.configs').setup{
+--     ensure_installed = { "c", "lua", "python", "javascript", "typescript", "html", "css", "json", "bash", "dockerfile", "go", "java", "toml", "vim", "yaml" },
+--
+--     sync_install = false,
+--     auto_install = true,
+--     highlight = {
+--         enable = true,
+--     },
+-- }
+require('nvim-treesitter').install { "c", "lua", "python", "javascript", "typescript", "html", "css", "json", "bash", "dockerfile", "go", "java", "toml", "vim", "yaml" }
