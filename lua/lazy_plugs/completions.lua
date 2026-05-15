2b2bffac7aea048e1c6c672c6fcf99f69f7e7383
saiakat <maxstender1@gmail.com>
2026-05-15 12:07:19 +0200

    updated lazy snip

--- lua/lazy_plugs/completions.lua
+++ lua/lazy_plugs/completions.lua
@@ -2,6 +2,12 @@ return {
   { 'hrsh7th/nvim-cmp' },
   { 'hrsh7th/cmp-nvim-lsp' },
   { 'saadparwaiz1/cmp_luasnip' },
-  { 'L3MON4D3/LuaSnip' },
+  {
+    "L3MON4D3/LuaSnip",
+    -- follow latest release.
+    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!).
+    build = "make install_jsregexp"
+  },
   { 'rafamadriz/friendly-snippets' },
 }
