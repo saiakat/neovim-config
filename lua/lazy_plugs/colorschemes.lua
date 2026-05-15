6f54bbd83a3cb926315591729b0da290342fef15
saiakat <maxstender1@gmail.com>
2026-05-15 11:52:55 +0200

    some maintenance

--- lua/lazy_plugs/colorschemes.lua
+++ lua/lazy_plugs/colorschemes.lua
@@ -3,5 +3,13 @@ return {
   -- my favorite colorscheme. Colorscheme on startup can be changed in lua/colorscheme.lua
   { 'rose-pine/neovim', as = 'rose-pine' },
   -- treesitte not a colorschmeme. But for better syntax highlighting
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+  {
+    'nvim-treesitter/nvim-treesitter',
+    lazy=false,
+    branch='main',
+    build = ':TSUpdate',
+    config = function()
+      require("plugins_config.treesitter")
+    end,
+  },
 }
--- lua/plugins_config/lsp.lua
+++ lua/plugins_config/lsp.lua
@@ -6,6 +6,7 @@ local servers = {
   "pyright",
   "lua_ls",
   "yamlls",
+  "jsonls",
 }
 local lsp = vim.lsp
 
