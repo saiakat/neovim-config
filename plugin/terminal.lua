e9f4513ffc440ec745e02ee343bcaeec3680155d
stenderm <max.stender@dw.com>
2026-05-18 14:00:16 +0200

    Create more buffers and jump to next buffer

--- plugin/terminal.lua
+++ plugin/terminal.lua
@@ -5,14 +5,10 @@ vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>")
 
 local state = {
   floating = {
-    buffers = {
-      buf1 = -1,
-      buf2 = -1,
-    },
+    buffers = { -1, -1 },
     win = -1,
   },
   current_buf = -1,
-  next_buf = -1,
 }
 
 local function create_float(opts)
@@ -26,13 +22,13 @@ local function create_float(opts)
 
   -- validate/create each named buffer in place
   local buffers = opts.buffers
-  for key, buf in pairs(buffers) do
+  for i, buf in ipairs(buffers) do
     if not vim.api.nvim_buf_is_valid(buf) then
-      buffers[key] = vim.api.nvim_create_buf(false, true)
+      buffers[i] = vim.api.nvim_create_buf(false, true)
     end
   end
 
-  local win = vim.api.nvim_open_win(buffers.buf1, true, {
+  local win = vim.api.nvim_open_win(buffers[1], true, {
     relative = "editor",
     width    = width,
     height   = height,
@@ -54,23 +50,38 @@ end
 local function toggle_terminal ()
   if not vim.api.nvim_win_is_valid(state.floating.win) then
     state.floating = create_float({ buffers = state.floating.buffers })
-    to_terminal(state.floating.buffers.buf1)
-    state.current_buf = state.floating.buffers.buf1
+    to_terminal(state.floating.buffers[1])
+    state.current_buf = 1
   else
     vim.api.nvim_win_hide(state.floating.win)
   end
 end
 
-local function switch_buf (buf, next)
-  next = next or false
-  vim.api.nvim_set_current_buf(buf)
-  to_terminal(buf)
+local function switch_buf (buf, get_next)
+  get_next = get_next or false
+
+  if get_next then
+    if next(state.floating.buffers, buf) then
+      buf = buf + 1
+    else
+      buf = 1
+    end
+  end
+
+  local buffer = state.floating.buffers[buf]
+  vim.api.nvim_set_current_buf(buffer)
+  to_terminal(buffer)
   state.current_buf = buf
 end
 
+local function create_buffer () 
+  local i = #state.floating.buffers + 1
+  state.floating.buffers[i] = vim.api.nvim_create_buf(false, true)
+end
 
 vim.api.nvim_create_user_command("Floaterminal", toggle_terminal, {})
 vim.keymap.set({"n", "t" }, "<leader>tt", toggle_terminal)
-vim.keymap.set({"n", "t" }, "<leader>1", function() switch_buf(state.floating.buffers.buf1) end)
-vim.keymap.set({"n", "t" }, "<leader>2", function() switch_buf(state.floating.buffers.buf2) end)
+vim.keymap.set({"n", "t" }, "<leader>1", function() switch_buf(1, false) end)
+vim.keymap.set({"n", "t" }, "<leader>2", function() switch_buf(2, false) end)
 vim.keymap.set({"n", "t" }, "<leader>n", function() switch_buf(state.current_buf, true) end)
+vim.keymap.set({"n", "t" }, "<leader>b", function() create_buffer() end)
